4da252b503654637af93d579c2a9f2afd9b3ac37
James <jamiesmith3979@gmail.com>
2023-11-11 23:58:47 +0000

    Updated

--- Personal project/app.py
+++ Personal project/app.py
@@ -4,14 +4,17 @@ import requests
 api_key = '30d4741c779ba94c470ca1f63045390a'
 
 app = Flask(__name__)
-
+# declaring the methods that we are going to use which are get and post, get being used to get the data
+# and then post to post the data onto the
 @app.route('/', methods=['GET', 'POST'])
 def home():
     weather = None
     temp = None
-
+    # if the request method is equal to post then we will request the form from the html which in this case
+    # is city
     if request.method == 'POST':
         user_input = request.form['city']
+        # we then use the 'user_input' which is the city to find the correct city for the weather
         weather_data = requests.get(
             f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}"
         )
@@ -22,8 +25,8 @@ def home():
         # else it will return the weather and temp data
             weather = weather_data.json()['weather'][0]['main']
             temp = weather_data.json()['main']['temp']
-        # we then render the weather data and temp data
-    return render_template("index.html",weather=weather, temp=temp, content= [weather, temp])
+        # we then render the weather data and temp data into the html allowing us to use it in our html code
+    return render_template("index.html",weather=weather, temp=temp)
 
 if __name__ == '__main__':
     app.run()
